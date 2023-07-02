b9dff6cda9ab7ee952d4d4274634a5f5382ebacd
PreethaPinto <preethacp@gmail.com>
2023-07-03 01:37:40 +1000

    Establish connection with backend

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
 const express = require('express');
+// const Connection = require('tedious').Connection;
+// const Request = require('tedious').Request;
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const sql = require('mssql');
+const sql = require('mssql/msnodesqlv8');
 const app = express();
+const port = 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
-//config for your database
+//config for database
 const sqlConfig = {
-  user: 'preethacp',
-  password: 'Rf6PVyj7B8!PNcT',
-  server: 'preethacp.database.windows.net',
-  database: 'HIGHSTREETGYM',
+  username: 'sa',
+  password: '1234',
+  server: 'localhost\\SQLEXPRESS',
+  database: 'high_street_gym',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true,
+  },
 };
+
+app.get('/trainers', (req, res) => {
+  //connect to your database
+
+  try {
+    sql.connect(sqlConfig, (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      //create Request Object
+      const request = new sql.Request();
+
+      //query to database
+      request.query('SELECT * from trainer', (err, data) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        //send data as a response
+        res.send(data.recordset);
+      });
+    });
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 //Http GET for classes
 app.get('/classes', (req, res) => {
   //connect to database
+
   try {
     sql.connect(sqlConfig, (err) => {
       if (err) {
@@ -29,7 +67,7 @@ app.get('/classes', (req, res) => {
     const request = new sql.Request();
 
     //query to database
-    request.query('SELECT * from classes', (err, data) => {
+    request.query('SELECT * from class', (err, data) => {
       if (err) {
         console.log(err);
         return;
@@ -37,35 +75,6 @@ app.get('/classes', (req, res) => {
 
       //send the data as response
       res.send(data.recordset);
-    });
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-//Http GET for trainers
-app.get('/trainers', (req, res) => {
-  //connect to your database
-  try {
-    sql.connect(sqlConfig, (err) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      //create Request Object
-      const request = new sql.Request();
-
-      //query to database
-      request.query('SELECT * from trainers', (err, data) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-
-        //send data as a response
-        res.send(data.recordset);
-      });
     });
   } catch (err) {
     res.send(err);
@@ -86,7 +95,7 @@ app.get('./blogs', (req, res) => {
       const request = new sql.Request();
 
       //query to database
-      request.query('SELECT * from blogs', (err, data) => {
+      request.query('SELECT * from blog', (err, data) => {
         if (err) {
           console.log(err);
           return;
@@ -100,52 +109,114 @@ app.get('./blogs', (req, res) => {
   }
 });
 
-//http POST for new members
-app.post('/member', (req, res) => {
-  try {
-    //Connect to database
-    sql.connect(sqlConfig, () => {
-      const request = new sql.Request();
+// //Http GET for trainers
+// app.get('/trainers', (req, res) => {
+//   //connect to your database
 
-      const { firstName, lastName, phoneNumber, emailId, password } = req.body;
+//   try {
+//     sql.connect(sqlConfig, (err) => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
 
-      let stringRequest = `INSERT INTO member(
-      firstName,
-      lastName,
-      phoneNumber,
-      emailId,
-      password
-      ) 
+//       //create Request Object
+//       const request = new sql.Request();
 
-      OUTPUT Inserted.memberId 
+//       //query to database
+//       request.query('SELECT * from trainer', (err, data) => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
 
-      VALUES (
-        @firstName,
-        @lastName,
-        @phoneNumber,
-        @emailId,
-        @password       
-      )`;
-      request.input('firstName', firstName);
-      request.input('lastName', lastName);
-      request.input('phoneNumber', phoneNumber);
-      request.input('emailId', emailId);
-      request.input('password', password);
+//         //send data as a response
+//         res.send(data.recordset);
+//       });
+//     });
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
 
-      request.query(stringRequest, (err, data) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
+// // //Http GET for blogs
+// // app.get('./blogs', (req, res) => {
+// //   try {
+// //     //connect to database
+// //     sql.connect(sqlConfig, (err) => {
+// //       if (err) {
+// //         console.log(err);
+// //         return;
+// //       }
 
-        res.send(JSON.stringify(data));
-      });
-    });
-  } catch (err) {
-    res.send(err);
-  }
-});
+// //       //create Request object
+// //       const request = new sql.Request();
 
-app.listen(8080, function () {
-  console.log('Listening to port 8080');
+// //       //query to database
+// //       request.query('SELECT * from blog', (err, data) => {
+// //         if (err) {
+// //           console.log(err);
+// //           return;
+// //         }
+// //         //send data as response
+// //         res.send(data);
+// //       });
+// //     });
+// //   } catch (err) {
+// //     res.send(err);
+// //   }
+// // });
+
+// // //http POST for new members
+// // app.post('/member', (req, res) => {
+// //   try {
+// //     //Connect to database
+// //     sql.connect(sqlConfig, () => {
+// //       const request = new sql.Request();
+
+// //       const { firstName, lastName, phoneNumber, emailId, password } = req.body;
+
+// //       let stringRequest = `INSERT INTO member(
+// //       firstName,
+// //       lastName,
+// //       phoneNumber,
+// //       emailId,
+// //       password
+// //       )
+
+// //       OUTPUT Inserted.memberId
+
+// //       VALUES (
+// //         @firstName,
+// //         @lastName,
+// //         @phoneNumber,
+// //         @emailId,
+// //         @password
+// //       )`;
+// //       request.input('firstName', firstName);
+// //       request.input('lastName', lastName);
+// //       request.input('phoneNumber', phoneNumber);
+// //       request.input('emailId', emailId);
+// //       request.input('password', password);
+
+// //       request.query(stringRequest, (err, data) => {
+// //         if (err) {
+// //           console.log(err);
+// //           return;
+// //         }
+
+// //         res.send(JSON.stringify(data));
+// //       });
+// //     });
+// //   } catch (err) {
+// //     res.send(err);
+// //   }
+// // });
+
+// app.listen(port, function () {
+//   console.log(`Listening to port ${port}`);
+// });
+
+app.listen(port, function () {
+  console.log(`Listening to port ${port}`);
 });
