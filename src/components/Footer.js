a651e6a5e3d0a7ee59ea716966c8f8d13207b419
PreethaPinto <preethacp@gmail.com>
2023-07-19 22:49:56 +1000

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>High Street Gym &copy; 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
