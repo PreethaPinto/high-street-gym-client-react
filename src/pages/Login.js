2215b864781dee3e18a226dae439b82db4562fdc
PreethaPinto <preethacp@gmail.com>
2023-07-20 23:31:11 +1000

    Changed styling to sign up and login pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,95 +1,55 @@
-import { Link, Navigate, Form, json, redirect } from 'react-router-dom';
+import { Link, Form, json, useActionData } from 'react-router-dom';
 
-import { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 import classes from './Login.module.css';
 
-const LoginPage = (props) => {
+const LoginPage = () => {
+  const data = useActionData();
   const userRef = useRef();
-
-  // const [formValues, setFormValues] = useState({ username: '', password: '' });
-  // const [formErrors, setFormErrors] = useState({});
-
-  //const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     userRef.current.focus();
   }, []);
-
-  // const submitHandler = (event) => {
-  //   event.preventDefault();
-  //   setFormErrors(validateForm(formValues));
-
-  //   //setSuccess(true);
-  // };
-
-  // const inputChangeHandler = (event) => {
-  //   const { id, value } = event.target;
-  //   setFormValues({ ...formValues, [id]: value });
-  // };
-
-  // const validateForm = (values) => {
-  //   let errors = {};
-
-  //   if (!values.username) {
-  //     errors.username = 'Username is required!';
-  //   }
-
-  //   if (!values.password) {
-  //     errors.password = 'Password required!';
-  //   }
-  //   return errors;
-  // };
 
   return (
     <>
       {/* {success ? (
         <Navigate to='/'></Navigate>
       ) : ( */}
-      <section>
-        <div className={classes['login-container']}>
-          <div className={classes['login-form']}>
-            <h3 className={classes.login}>LOGIN</h3>
+      <div className={classes['login-container']}>
+        <h1 className={classes.login}>LOGIN</h1>
+        <Form method='post'>
+          <input
+            type='text'
+            id='username'
+            name='username'
+            placeholder='Username'
+            ref={userRef}
+            className={classes['login-input']}
+          />
 
-            <Form method='post'>
-              <div className={classes['login-inputbox']}>
-                <label htmlFor='username'>Username</label>
-                <input
-                  type='text'
-                  id='username'
-                  ref={userRef}
-                  className={classes['login-input']}
-                />
-                {/* {formErrors.username && (
-                  <p style={{ color: 'red' }}>{formErrors.username}</p>
-                )} */}
-              </div>
+          <input
+            type='text'
+            id='password'
+            placeholder='Password'
+            name='password'
+            // onChange={(event) =>
+            //   inputChangeHandler('password', event.target.value)
+            // }
+            className={classes['login-input']}
+          />
 
-              <div className={classes['login-inputbox']}>
-                <label htmlFor='password'>Password</label>
-                <input
-                  type='text'
-                  id='password'
-                  // onChange={(event) =>
-                  //   inputChangeHandler('password', event.target.value)
-                  // }
-                  className={classes['login-input']}
-                />
-                {/* {formErrors.password && (
-                  <p style={{ color: 'red' }}>{formErrors.password}</p>
-                )} */}
-              </div>
-
-              <button type='submit' className={classes['btn-login']}>
-                Login
-              </button>
-              <p className={classes.register}>
-                Don't have an account? <Link to='/signup'>Register</Link>
-              </p>
-            </Form>
-          </div>
-        </div>
-      </section>
+          <button type='submit' className={classes['btn-login']}>
+            Login
+          </button>
+          <p>This is an error</p>
+          <span className={classes.register}>
+            Don't have an account? <br />
+            <Link to='/signup'>Register</Link>
+          </span>
+        </Form>
+      </div>
     </>
   );
 };
@@ -113,11 +73,14 @@ export const loginAction = async ({ request }) => {
       body: JSON.stringify(loginData),
     });
 
+    const { username, password } = loginData;
+
     if (!response.ok) {
       throw json({ message: 'Could not login' }, { status: 500 });
     }
 
     const responseData = await response.json();
+
     console.log(responseData);
   } catch (err) {
     console.log('Error:', err);
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,117 +7,93 @@ const SignupPage = () => {
 
   return (
     <div className={classes['signup-container']}>
-      <div className={classes['signup-form']}>
-        <h2 className={classes.signup}>SIGN UP</h2>
-        <Form method='post'>
-          {data && data.userValidate && (
-            <ul>
-              {data.userValidate.map((err) => (
-                <li key={err}>{err}</li>
-              ))}
-            </ul>
-          )}
-          <div>
-            <label htmlFor='first_name'>First Name</label>
-            <input
-              id='first_name'
-              name='first_name'
-              type='text'
-              placeholder='First Name'
-              className={classes['signup-input']}
-            />
-            {/* {errors.first_name && (
+      <h2 className={classes.signup}>SIGN UP</h2>
+      <Form method='post'>
+        {data && data.userValidate && (
+          <ul>
+            {data.userValidate.map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        <input
+          id='first_name'
+          name='first_name'
+          type='text'
+          placeholder='First Name'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.first_name && (
               <p style={{ color: 'red' }}>{errors.first_name}</p>
             )} */}
-          </div>
-          <div>
-            <label htmlFor='last_name'>Last Name</label>
-            <input
-              id='last_name'
-              type='text'
-              name='last_name'
-              placeholder='Last Name'
-              className={classes['signup-input']}
-            />
-            {/* {errors.last_name && (
+        <input
+          id='last_name'
+          type='text'
+          name='last_name'
+          placeholder='Last Name'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.last_name && (
               <p style={{ color: 'red' }}>{errors.last_name}</p>
             )} */}
-          </div>
-          <div>
-            <label htmlFor='phone_number'>Phone</label>
-            <input
-              id='phone_number'
-              type='text'
-              name='phone_number'
-              placeholder='Phone Number'
-              className={classes['signup-input']}
-            />
-            {/* {errors.phone_number && <p style={{ color: 'red' }}>{errors.phone_number}</p>} */}
-          </div>
-          <div>
-            <label htmlFor='email_id'>Email</label>
-            <input
-              id='email_id'
-              type='text'
-              name='email_id'
-              placeholder='Email'
-              className={classes['signup-input']}
-            />
-            {/* {errors.email_id && <p style={{ color: 'red' }}>{errors.email_id}</p>} */}
-          </div>
-          <div>
-            <label htmlFor='username'>Username</label>
-            <input
-              id='username'
-              type='text'
-              name='username'
-              placeholder='Username'
-              className={classes['signup-input']}
-            />
-            {/* {errors.username && (
+        <input
+          id='phone_number'
+          type='text'
+          name='phone_number'
+          placeholder='Phone Number'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.phone_number && <p style={{ color: 'red' }}>{errors.phone_number}</p>} */}
+        <input
+          id='email_id'
+          type='text'
+          name='email_id'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.email_id && <p style={{ color: 'red' }}>{errors.email_id}</p>} */}
+        <input
+          id='username'
+          type='text'
+          name='username'
+          placeholder='Username'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.username && (
               <p style={{ color: 'red' }}>{errors.username}</p>
             )} */}
-          </div>
 
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input
-              id='password'
-              type='text'
-              name='password'
-              placeholder='Password'
-              className={classes['signup-input']}
-            />
-            {/* {errors.password && (
+        <input
+          id='password'
+          type='text'
+          name='password'
+          placeholder='Password'
+          required
+          className={classes['signup-input']}
+        />
+        {/* {errors.password && (
               <p style={{ color: 'red' }}>{errors.password}</p>
             )} */}
-          </div>
-          <div>
-            <label htmlFor='confirm_password'>Confirm Password</label>
-            <input
-              id='confirm_password'
-              type='text'
-              name='confirm_password'
-              placeholder='Confirm Password'
-              className={classes['signup-input']}
-            />
-            {/* {errors.confirm_password && (
-              <p style={{ color: 'red' }}>{errors.confirm_password}</p>
-            )} */}
-          </div>
-          <button type='submit' className={classes['btn-signup']}>
-            Sign Up
-          </button>
-        </Form>
-        <p>
-          Already a member? <Link to='/login'>Login</Link>
-        </p>
-      </div>
+
+        <button type='submit' className={classes['btn-signup']}>
+          Sign Up
+        </button>
+        <span>
+          Already a member? <br /> <Link to='/login'>Login</Link>
+        </span>
+      </Form>
     </div>
   );
 };
 
 export default SignupPage;
+
+const xmlbuilder = require('xmlbuilder');
+const fetch = require('node-fetch');
 
 export const signupAction = async ({ request }) => {
   const data = await request.formData();
@@ -129,16 +105,24 @@ export const signupAction = async ({ request }) => {
     email_id: data.get('email_id'),
     username: data.get('username'),
     password: data.get('password'),
-    confirm_password: data.get('confirm_password'),
   };
   console.log(signupData);
+
+  // Convert the signupData object to XML format
+  const xmlData = xmlbuilder.create('signupData');
+  for (const [key, value] of Object.entries(signupData)) {
+    xmlData.ele(key, value);
+  }
+  const xmlString = xmlData.end({ pretty: true });
+
+  console.log(xmlString);
 
   const response = await fetch('http://localhost:8080/signup', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/xml',
     },
-    body: JSON.stringify(signupData),
+    body: xmlString,
   });
 
   if (response.status === 400) {
@@ -149,3 +133,20 @@ export const signupAction = async ({ request }) => {
     throw json({ message: 'Could not submit form' }, { status: 500 });
   }
 };
+
+//   const response = await fetch('http://localhost:8080/signup', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(signupData),
+//   });
+
+//   if (response.status === 400) {
+//     return response;
+//   }
+
+//   if (!response.ok) {
+//     throw json({ message: 'Could not submit form' }, { status: 500 });
+//   }
+// };
