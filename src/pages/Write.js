79db29aac2198ee995470c52087d25ac4d5faaf5
PreethaPinto <preethacp@gmail.com>
2023-07-21 23:23:26 +1000

    Add text box and text editor using react quill to add blog post

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,12 +1,45 @@
+import ReactQuill from 'react-quill';
+import { useState } from 'react';
+
 import classes from './Write.module.scss';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+  const [value, setValue] = useState('');
+
   return (
-    <div className={classes.write}>
-      <div className={classes.content}>content</div>
-      <div className={classes.menu}>
-        <div className={classes.item}>I1</div>
-        <div className={classes.item}>I2</div>
+    <div className={classes['write-wrapper']}>
+      <div className={classes.write}>
+        <div className={classes.content}>
+          <input type='text' placeholder='Title' />
+          <div className={classes['editor-container']}>
+            <ReactQuill
+              className={classes.editor}
+              theme='snow'
+              value={value}
+              onChange={setValue}
+            />
+          </div>
+        </div>
+        <div className={classes.menu}>
+          <div className={classes.item}>
+            <h1>Publish</h1>
+            <span>
+              <b>Status: </b> Draft
+            </span>
+            <span>
+              <b>Visibility: </b> Public
+            </span>
+            <input style={{ display: 'none' }} type='file' name='' id='file' />
+            <label htmlFor='file' className={classes.file}>
+              Upload Image
+            </label>
+            <div className={classes.buttons}>
+              <button>Save as a draft</button>
+              <button>Update</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
